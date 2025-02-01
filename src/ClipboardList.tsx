3397a4fe4d99549296d792f4447e0f53537a8796
gustavogamboa96 <gustavo.gamboa96@gmail.com>
2025-02-01 09:38:33 -0500

    first commit

---
+++ src/ClipboardList.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import {DragIndicator, Close} from '@mui/icons-material';
+
+declare global {
+  interface Window {
+    electron: {
+      getClipboardData: () => void;
+      onClipboardUpdate: (callback: (data: string[]) => void) => void;
+      onClipboardData: (callback: (data: string[]) => void) => void; 
+      writeToClipboard: (text: string) => void;
+      closeApp: () => void;
+    };
+  }
+}
+
+const ClipboardList: React.FC = () => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [clipboardItems, setClipboardItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    window.electron.getClipboardData();
+    window.electron.onClipboardUpdate((data) => setClipboardItems(data));
+    window.electron.onClipboardData((data) => setClipboardItems(data));
+  }, []);
+
+  const handleListItemClick = (index: number) => {
+    const rightIndex=clipboardItems.length - 1 - index
+    setSelectedIndex(rightIndex);
+    const textToCopy = clipboardItems[rightIndex];
+    if (textToCopy) {
+      window.electron.writeToClipboard(textToCopy);
+      console.log('Copied text:', textToCopy);
+    }
+  };
+
+  const handleCloseApp = () => {
+    window.electron.closeApp(); // Call the closeApp function
+  };
+  
+
+  return (
+    <Box sx={{ width: '100%', backgroundColor: 'gray'}}>
+      <DragIndicator className='drag-button' sx={{ color: "black", bgcolor: 'gray'}}/>
+      <Close className='close-button' onClick={handleCloseApp} sx={{ color: "black", bgcolor: 'gray' }}/>
+      <List component="nav" aria-label="clipboard contents" sx={{bgColor: "gray"}}>
+        {clipboardItems.slice().reverse().map((item, index) => (
+          <ListItemButton
+            key={index}
+            selected={selectedIndex === index}
+            onClick={() => handleListItemClick(index)}
+            sx={{backgroundColor: "gray"}}
+          >
+            <ListItemText primary={item} sx={{ color: "white" }}/>
+          </ListItemButton>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+};
+
+export default ClipboardList;
